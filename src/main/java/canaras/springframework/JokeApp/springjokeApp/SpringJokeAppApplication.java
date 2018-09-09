add786dbeaf885f755506693dce68413350b3cd0
arascan35 <arascan.35@outlook.com>
2018-09-09 15:10:59 +0300

    JokeApp has been created.

---
+++ src/main/java/canaras/springframework/JokeApp/springjokeApp/SpringJokeAppApplication.java
@@ -0,0 +1,12 @@
+package canaras.springframework.JokeApp.springjokeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokeAppApplication.class, args);
+	}
+}
---
+++ src/test/java/canaras/springframework/JokeApp/springjokeApp/SpringJokeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package canaras.springframework.JokeApp.springjokeApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJokeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
